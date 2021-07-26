dc728265f61b22a4221e40375f3480309e68fe03
wnipuna <nipuna.r3@gmail.com>
2021-07-26 14:57:04 +0530

    second commit

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,7 +4,7 @@ const Blog =  require('../models/blog');
 const blog_index = (req, res)=>{
     Blog.find().sort({ createdAt: -1 })
         .then((result) => {
-            res.render('index', { title: 'All blogs', blogs: result });
+            res.render('blogs/index', { title: 'All blogs', blogs: result });
         })
         .catch((err) => {
             console.log(err);
@@ -12,7 +12,7 @@ const blog_index = (req, res)=>{
 }
 
 const blog_create_get = (req, res)=>{
-    res.render('create', { title: 'Create new blog' });
+    res.render('blogs/create', { title: 'Create new blog' });
 }
 
 const blog_create_post = (req, res)=>{
@@ -30,7 +30,7 @@ const blog_details = (req, res)=>{
     const id = req.params.id;
     Blog.findById(id)
         .then((result) => {
-            res.render('details', { blog: result, title: 'Blog Details' })
+            res.render('blogs/details', { blog: result, title: 'Blog Details' })
         })
         .catch((err) => {
             console.log(err);
